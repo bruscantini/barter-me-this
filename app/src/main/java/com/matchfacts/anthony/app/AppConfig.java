56306e550aafc22ae2e55718a20aa9c98735cf13
Anthony Jose Bruscantini <ajbruscantini@gmail.com>
2016-04-22 10:12:49 -0400

    Added HOST variable to AppConfig to easily change the host where the server is.

--- app/src/main/java/com/matchfacts/anthony/app/AppConfig.java
+++ app/src/main/java/com/matchfacts/anthony/app/AppConfig.java
@@ -4,17 +4,21 @@ package com.matchfacts.anthony.app;
  * Created by Anthony on 3/31/2016.
  */
 public class AppConfig {
+
+    private static final String HOST = "http://192.168.1.10:8888/";
+    // private static final String HOST = "http://matchfacts.asuscomm.com:8888/";
+
     // Server user login url
-    public static String URL_LOGIN = "http://matchfacts.asuscomm.com:8888/barter-me-that/login.php";
+    public static final String URL_LOGIN = HOST + "barter-me-that/login.php";
     // Server user register url
-    public static String URL_REGISTER = "http://matchfacts.asuscomm.com:8888/barter-me-that/register.php";
+    public static final String URL_REGISTER = HOST + "barter-me-that/register.php";
     // url to get all products list
-    public static String URL_ALL_PRODUCTS = "http://matchfacts.asuscomm.com:8888/android_connect/get_all_products.php";
+    public static final String URL_ALL_PRODUCTS = HOST + "android_connect/get_all_products.php";
     // single product url
-    public static final String URL_PRODUCT_DETAILS = "http://matchfacts.asuscomm.com:8888/android_connect/get_product_details.php";
+    public static final String URL_PRODUCT_DETAILS = HOST + "android_connect/get_product_details.php";
     // url to update product
-    public static final String URL_UPDATE_PRODUCT = "http://matchfacts.asuscomm.com:8888/android_connect/update_product.php";
+    public static final String URL_UPDATE_PRODUCT = HOST + "android_connect/update_product.php";
     // url to delete product
-    public static final String URL_DELETE_PRODUCT = "http://matchfacts.asuscomm.com:8888/android_connect/delete_product.php";
+    public static final String URL_DELETE_PRODUCT = HOST + "android_connect/delete_product.php";
 
 }
